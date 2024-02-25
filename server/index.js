4738501c00824af2a31f9d024ed48d33487d1032
rohan09-raj <rajrohan1914@gmail.com>
2024-02-25 19:26:52 +0530

    refactor: replaced ws with socket.io

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from "ws";
+import { Server } from "socket.io";
 import http from "http";
 import { v4 as uuidv4 } from "uuid";
 import { handleMessage, handleDisconnect } from "./handlers/handlers.js";
@@ -10,16 +10,24 @@ let canvasData = null;
 let userActivity = [];
 
 const server = http.createServer();
-const wsServer = new WebSocketServer({ server });
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-wsServer.on("connection", function (connection) {
+io.on("connection", function (connection) {
   const userId = uuidv4();
   console.log("Recieved a new connection");
   clients[userId] = connection;
   console.log(`${userId} connected.`);
-  connection.on("message", (message) => handleMessage(message, userId, users, clients, userActivity, canvasData));
-  connection.on("close", () => handleDisconnect(userId, clients, users, userActivity));
+  connection.on("message", (message) =>
+    handleMessage(message, userId, users, clients, userActivity, canvasData)
+  );
+  connection.on("close", () =>
+    handleDisconnect(userId, clients, users, userActivity)
+  );
 });
