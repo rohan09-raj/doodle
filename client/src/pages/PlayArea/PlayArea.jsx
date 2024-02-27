f51c8804ad018502f4d1be85a1f3647ab4ec7a34
rohan09-raj <rajrohan1914@gmail.com>
2024-02-27 08:38:27 +0530

    feat: handle wait event for 1 user

--- client/src/pages/PlayArea/PlayArea.jsx
+++ client/src/pages/PlayArea/PlayArea.jsx
@@ -1,17 +1,37 @@
+import PropTypes from "prop-types";
 import Users from "../../components/Users/Users";
 import Canvas from "../../components/Canvas/Canvas";
 import History from "../../components/History/History";
 
 import styles from "./PlayArea.module.css";
+import { useEffect } from "react";
+import { SOCKET_EVENTS } from "../../constants/constants";
+import Card from "../../components/basic/Card/Card";
 
-const PlayArea = () => {
-  return (
+const PlayArea = ({ socketRef, wait, setWait }) => {
+  useEffect(() => {
+    socketRef.current.on(SOCKET_EVENTS.WAIT, (value) => {
+      setWait(value);
+    });
+  }, []);
+
+  return wait ? (
+    <Card>
+      <h2>Waiting for players to join...</h2>
+    </Card>
+  ) : (
     <div className={styles.area}>
       <Users />
       <Canvas />
       <History />
     </div>
   );
+};
+
+PlayArea.propTypes = {
+  socketRef: PropTypes.object,
+  setWait: PropTypes.func,
+  wait: PropTypes.bool,
 };
 
 export default PlayArea;
