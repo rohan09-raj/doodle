0c8d290ab5482350f32f8ac92f72f4bfeefa075b
rohan09-raj <rajrohan1914@gmail.com>
2024-03-02 18:08:20 +0530

    chore: added color constants

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -27,3 +27,24 @@ export const SOCKET_EVENTS = {
   CLEAR: "clear",
   UPDATE_SCORE: "updateScore",
 };
+
+export const COLORS = [
+  '#000000',
+  '#FFFFFF',
+  '#C0C0C0',
+  '#808080',
+  '#FFA500',
+  '#A52A2A',
+  '#800000',
+  '#008000',
+  '#808000',
+  '#FF0000',
+  '#00FFFF',
+  '#0000FF',
+  '#0000A0',
+  '#ADD8E6',
+  '#800080',
+  '#FFFF00',
+  '#00FF00',
+  '#FF00FF'
+]
